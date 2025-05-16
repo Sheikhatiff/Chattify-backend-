6a276bcb6fdaf9870bdacbce6eae459ded5dec4e
Sheikhatiff <atif55sheikh@gmail.com>
2025-05-16 22:32:38 +0500

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -18,14 +18,15 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 app.use(cookieParser());
-app.use(
-  cors()
-  //   {
-  //   origin: process.env.CLIENT_URL,
-  //   credentials: true,
-  // }
-);
-
+// app.use(
+//   cors()
+//   //   {
+//   //   origin: process.env.CLIENT_URL,
+//   //   credentials: true,
+//   //   j
+//   // }
+// );
+app.use(cors());
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
 
