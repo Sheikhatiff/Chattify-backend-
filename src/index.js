00bec97eff3365e3b9cb74516a84ddce26b365bf
Sheikhatiff <atif55sheikh@gmail.com>
2025-05-16 22:26:45 +0500

    updating

--- src/index.js
+++ src/index.js
@@ -19,10 +19,11 @@ app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 app.use(cookieParser());
 app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  })
+  cors()
+  //   {
+  //   origin: process.env.CLIENT_URL,
+  //   credentials: true,
+  // }
 );
 
 app.use("/api/auth", authRouter);
